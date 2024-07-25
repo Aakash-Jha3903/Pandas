89569ab2a50e6eaf9e4dd0d4cb02d76428e22f7a
Aakash <aakashjha343@gmail.com>
2024-07-25 23:33:11 +0530

    pandas practise

--- String_Method_Pandas.py
+++ String_Method_Pandas.py
@@ -9,17 +9,17 @@ data = {
 df = pd.DataFrame(data)
 print("Display All Data:\n", df)
 
-# Convert to lowercase
+"""Convert to lowercase"""
 #print("\nLowercase cities:\n", df['city'].str.lower())
 
-# Convert to Uppercase
+"""Convert to Uppercase"""
 #print("\nUppercase cities:\n", df['city'].str.upper())
 
-# Length of String
+"""Length of String"""
 #print("\nString Length cities:\n", df['city'].str.len())
 
-# Extract first three characters
+"""Extract first three characters"""
 #print("\nCity substrings (first 3 characters):\n", df['city'].str[:3])
 
-# Replace substrings
+"""Replace substrings"""
 #print("\nCities with 'a' replaced by 'A':\n", df['city'].str.replace('a', 'A'))
--- Value_Counts.py
+++ Value_Counts.py
@@ -12,18 +12,18 @@ data = {
 df = pd.DataFrame(data)
 print("Display All Data:\n", df)
 
-# Use value_counts to count unique values in the 'city' column
+"""Use value_counts to count unique values in the 'city' column"""
 city_counts = df['city'].value_counts()
 print("\nCity counts:\n", city_counts)
 
-# Use value_counts to count unique values in the 'name' column
+"""Use value_counts to count unique values in the 'name' column"""
 name_counts = df['name'].value_counts()
 print("\nName counts:\n", name_counts)
 
-# Use value_counts to count unique values in the 'age' column including NaN values
+"""Use value_counts to count unique values in the 'age' column including NaN values"""
 age_counts = df['age'].value_counts(dropna=False)
 print("\nAge counts including NaN:\n", age_counts)
 
-# Use value_counts with sorting by index
+"""Use value_counts with sorting by index"""
 city_counts_sorted = df['city'].value_counts().sort_index()
 print("\nCity counts sorted by index:\n", city_counts_sorted)
