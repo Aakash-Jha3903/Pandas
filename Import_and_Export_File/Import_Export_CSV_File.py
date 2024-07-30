530d49dab3d39ac0277703d844c14b7ff8178b6e
Aakash <aakashjha343@gmail.com>
2024-07-30 17:52:49 +0530

    Import and export csv files

---
+++ Import_and_Export_File/Import_Export_CSV_File.py
@@ -0,0 +1,16 @@
+# Import Export CSV File
+import pandas as pd
+import numpy as np
+
+data = {
+    'name': ['aj', 'shivam', 'mohit', 'mohit'],
+    'city': ['Madhubani', 'Madhubani', 'Mithila', 'Noida'],
+    'age': [28, 24, 24, np.nan],
+}
+df = pd.DataFrame(data)
+# Writing DataFrame to CSV file
+df.to_csv('Import_and_Export_File\sample.csv', index=False)  # Without index
+
+# Reading the CSV file back into a DataFrame
+df_read = pd.read_csv('sample.csv')
+print(df_read)
